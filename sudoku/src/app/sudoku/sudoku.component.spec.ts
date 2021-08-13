8fd1294999ed9b932860ee2f4f246f440104376d
Gurvinder Singh <gurayasaab@hotmail.clom>
2021-08-13 00:19:08 -0400

    update to version 11

--- sudoku/src/app/sudoku/sudoku.component.spec.ts
+++ sudoku/src/app/sudoku/sudoku.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SudokuComponent } from './sudoku.component';
 
@@ -6,7 +6,7 @@ describe('SudokuComponent', () => {
   let component: SudokuComponent;
   let fixture: ComponentFixture<SudokuComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SudokuComponent ]
     })
